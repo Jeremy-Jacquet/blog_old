e5a49e15f756c1f3a3c81f2d459a8c1f10b485bd
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-09-02 14:00:51 +0200

    Update Database.php
    Add:
    - const DB_HOST
    - const DB_HOST

--- App/library/BlogFram/Database.php
+++ App/library/BlogFram/Database.php
@@ -5,11 +5,16 @@ use \PDO;
 
 abstract class Database 
 {
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'blog_perso_2';
+    const DB_USER = 'root';
+    const DB_PSWD = '';
+
     private static $pdo;
 
     private function setPDO()
     {
-        self::$pdo = new PDO("mysql:host=".HOST.";dbname=".DB.";charset=utf8", USER_DB, PSWD_DB);
+        self::$pdo = new PDO("mysql:host=".self::DB_HOST.";dbname=".self::DB_NAME.";charset=utf8", self::DB_USER, self::DB_PSWD);
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
