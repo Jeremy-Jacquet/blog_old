d00e6321e81fc825b683171698f830ecd2a1e64d
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-09-02 11:54:19 +0200

    Update index.php
    Add:
    - require 'App/config/Database.php'

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
+require 'App/config/Database.php';
+
 try {
 
     if(empty($_GET['page']))
     {
         //Go to home
-        
+
     } else {
         throw new Exception("La page que vous recherchez n'existe pas.");
     }
