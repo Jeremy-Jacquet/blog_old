2fbc6a534dcfb0a85a84d66a01bd7d6fafd0e1bf
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-09-02 16:29:06 +0200

    Update config.php

--- App/config/config.php
+++ App/config/config.php
@@ -1,3 +1,19 @@
 <?php
 
 define("URL", str_replace("index.php", "",(isset($_SERVER['HTTPS']) ? "https" : "http")."://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']));
+
+define('HOME', 'accueil');
+define('POSTS', 'articles');
+define('CATEGORIES', 'categories');
+define('COMMENTS', 'commentaires');
+define('AUTHORS', 'auteurs');
+define('USERS', 'utilisateurs');
+define('PROFILE', 'profil');
+define('LOGIN', 'connexion');
+define('REGISTER', 'inscription');
+define('LOGOUT', 'deconnexion');
+define('ADMIN', 'admin');
+
+define("ADD", 'ajouter');
+define("UP", 'modifier');
+define("DEL", 'supprimer');
