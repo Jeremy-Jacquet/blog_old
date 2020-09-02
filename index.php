0b82ece8a4f653dfb4850030d07ec79ab686efac
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-09-02 11:32:00 +0200

    Create index.php close #3
    Add:
    - try/catch

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+try {
+
+    if(empty($_GET['page']))
+    {
+        //Go to home
+        
+    } else {
+        throw new Exception("La page que vous recherchez n'existe pas.");
+    }
+
+} catch(Exception $e) {
+    $error = $e->getMessage();
+}
