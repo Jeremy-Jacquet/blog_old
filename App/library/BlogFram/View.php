45d690499274634a60ed8ca3d10cc0ba8ed25143
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-09-08 17:10:58 +0200

    Create View.php
    View.php will redirect the user to the good page with the good title etc...

---
+++ App/library/BlogFram/View.php
@@ -0,0 +1,95 @@
+<?php
+namespace App\library\BlogFram;
+
+class View
+{
+
+    public function viewError()
+    {
+    }
+
+    public function viewAccess()
+    {
+    }
+
+    public function viewtHome()
+    {
+    }
+
+    public function viewAllArticles()
+    {
+    }
+
+    public function viewAllCategories()
+    {
+    }
+
+    public function viewArticle()
+    {
+    }
+
+    public function viewCategory()
+    {
+    }
+
+    public function viewRegister()
+    {
+    }
+
+    public function viewLogin()
+    {
+    }
+
+    public function viewAdmin()
+    {
+    }
+
+    public function viewAdminArticles()
+    {
+    }
+
+    public function viewAdminAddArticle()
+    {
+    }
+
+    public function viewAdminUpdateArticle()
+    {
+    }
+
+    public function viewAdminComments()
+    {
+    }
+
+    public function viewAdminAddComment()
+    {
+    }
+
+    public function viewAdminUpdateComment()
+    {
+    }
+
+    public function viewAdminCategories()
+    {
+    }
+
+    public function viewAdminAddCategory()
+    {
+    }
+
+    public function viewAdminUpdateCategory()
+    {
+    }
+
+    public function viewAdminAuthors()
+    {
+    }
+
+    public function viewAdminAddAuthor()
+    {
+    }
+
+    public function viewAdminUpdateAuthor()
+    {
+    }
+
+}
