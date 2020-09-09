d25898b3bd72118019f02a6a184f9e5921a5ccc0
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-09-09 11:17:51 +0200

    Update Router.php

--- App/library/BlogFram/Router.php
+++ App/library/BlogFram/Router.php
@@ -3,23 +3,44 @@ namespace App\library\BlogFram;
 
 use App\library\BlogFram\HTTPRequest;
 use App\library\BlogFram\HTTPResponse;
+use App\backend\controllers\BackendController;
+use App\frontend\controllers\FrontendController;
+use App\library\BlogFram\Route;
 
 class Router
 {
+    use Route;
 
     private $HTTPRequest;
     private $HTTPResponse;
+    private $controllerName;
     private $controller;
 
     public function __construct()
     {
         $this->HTTPRequest = new HTTPRequest();
         $this->HTTPResponse = new HTTPResponse($this->HTTPRequest->getGet(),$this->HTTPRequest->getPost());
-        $this->controller = $this->HTTPResponse->getController();
+        $this->controllerName = $this->HTTPResponse->getControllerName();
+        $this->getController();
     }
 
     public function run()
     {
+        if($this->verifyRoute(  $this->HTTPResponse->getAccess(),
+                                $this->HTTPResponse->getModule(),
+                                $this->HTTPResponse->getAction(),
+                                $this->HTTPResponse->getId())) {
+            $route = $this->getRouteByController($this->controllerName, $this->HTTPResponse->getModule());
+            $this->controller->$route($this->HTTPResponse->getAction(), $this->HTTPResponse->getId());
+        }
+    }
+
+    public function getController() {
+        if($this->controllerName === 'backend') {
+            $this->controller = new BackendController;
+        } elseif($this->controllerName === 'frontend') {
+            $this->controller = new FrontendController;
+        }
     }
 
 }
