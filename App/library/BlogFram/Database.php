89e12f759427f79b7b631ac7d77c8ab0dcd4d169
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-09-09 11:20:06 +0200

    Update Database.php
    Functions have been changed to be static so that managers no longer extend on the Database class.

--- App/library/BlogFram/Database.php
+++ App/library/BlogFram/Database.php
@@ -8,20 +8,20 @@ abstract class Database
     const DB_HOST = 'localhost';
     const DB_NAME = 'blog_perso_2';
     const DB_USER = 'root';
-    const DB_PSWD = 'root';
+    const DB_PSWD = '';
 
     private static $pdo;
 
-    private function setPDO()
+    private static function setPDO()
     {
         self::$pdo = new PDO("mysql:host=".self::DB_HOST.";dbname=".self::DB_NAME.";charset=utf8", self::DB_USER, self::DB_PSWD);
         self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     }
 
-    protected function getPDO()
+    public static function getPDO()
     {
         if(self::$pdo === null) {
-            $this->setPDO();
+            self::setPDO();
         }
         return self::$pdo;
     }
