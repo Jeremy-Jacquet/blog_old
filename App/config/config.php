28372736a40a32733a7e7e746ee3156cddede525
Jeremy Jacquet <j.jacquet.contact@gmail.com>
2020-09-22 11:10:45 +0200

    Update config.php

--- App/config/config.php
+++ App/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("URL", str_replace("index.php", "",(isset($_SERVER['HTTPS']) ? "https" : "http")."://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']));
+define("URL", str_replace("public/index.php", "",(isset($_SERVER['HTTPS']) ? "https" : "http")."://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']));
 
 define('HOME', 'accueil');
 define('POSTS', 'articles');
@@ -9,9 +9,11 @@ define('COMMENTS', 'commentaires');
 define('AUTHORS', 'auteurs');
 define('USERS', 'utilisateurs');
 define('PROFILE', 'profil');
+define('CONTACT', 'contact');
 define('LOGIN', 'connexion');
 define('REGISTER', 'inscription');
 define('LOGOUT', 'deconnexion');
+define('PUBLISH', 'publier');
 define('ADMIN', 'admin');
 
 define('DASHBOARD', 'dashboard');
@@ -19,3 +21,11 @@ define('DASHBOARD', 'dashboard');
 define("ADD", 'ajouter');
 define("UP", 'modifier');
 define("DEL", 'supprimer');
+
+define("ADMIN_ACCESS", 'admin');
+define("AUTHOR_ACCESS", 'author');
+define("USER_ACCESS", 'user');
+
+define('ERROR', 'error');
+
+define("COOKIE_PROTECT", "timer");
